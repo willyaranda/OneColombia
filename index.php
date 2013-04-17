a24865ef20994772b201691fcd7106e9873fe242
Mayccoll <mayccoll@gmail.com>
2013-04-16 21:05:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-  echo "Mozilla Colombia";
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mozilla Colombia</title>
+</head>
+<body>
+    <h1>Mozilla Colombia</h1>
+    <p>Test php</p>
+</body>
+</html>
