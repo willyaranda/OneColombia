01c75df5bbc4047ac91ef252ad26f1165a18e9d4
Mayccoll <mayccoll@gmail.com>
2013-04-19 10:42:56 -0500

    x

--- index.php
+++ index.php
@@ -1,14 +1,131 @@
-<?php
-?>
-
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Mozilla Colombia</title>
+	<meta charset="UTF-8">
+	<title> Mozilla Colombia |  </title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="main/css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="main/css/fonts.css" />
+	<link rel="stylesheet" href="//www.mozilla.org/tabzilla/media/css/tabzilla.css"  />
+	<link rel="stylesheet" type="text/css" media="screen" href="main/css/activate.css" />
+
+	<!-- JS -->
+
 </head>
 <body>
-    <h1>Mozilla Colombia</h1>
-    <p>Test php</p>
+
+<div id="wrap">
+
+	<header id="header">
+	<a href="http://www.mozilla.org/" id="tabzilla" data-locale="es-ES">mozilla</a>
+		<div id="logo-mc"></div>
+		<h1>Mozilla Colombia</h1>
+		<h2>Internet, Libre</h2>
+	</header>
+	<div id="mosaico">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+	<div id="content">
+	<div id="banner-news"></div>
+	<nav>
+		<ul>
+			<li><span class="box-one"></span><a href=""> Objetivo </a></li>
+			<li><span class="box-two"></span><a href=""> Comunidad </a></li>
+			<li><span class="box-tree"></span><a href=""> Web Abierta </a></li>
+			<li><span class="box-four"></span><a href=""> Blog </a></li>
+			<li><span class="box-five"></span><a href=""> Comunidades </a></li>
+			<li><span class="box-six"></span><a href=""> </a></li>
+		</ul>
+	</nav>
+	</div>
+
+	<div id="comunity"></div>
+
+	<div id="products"></div>
+
+	<footer id="colophon">
+		<div class="row">
+
+			<div class="footer-logo">
+				<a href="/en-US/">
+					<img alt="mozilla" src="//mozorg.cdn.mozilla.net/media/img/sandstone/footer-mozilla.png"></a>
+			</div>
+
+			<div class="footer-license">
+				<p>
+					Portions of this content are &copy;1998&ndash;2013 by individual mozilla.org contributors. Content available under a
+					<a href="/foundation/licensing/website-content.html">Creative Commons license</a>
+					.
+				</p>
+				<p>
+					<a href="/en-US/contribute/page/">Contribute to this page</a>
+				</p>
+			</div>
+			<ul class="footer-nav">
+				<li>
+					<a href="/en-US/about/contact.html#map-mountain_view">Contact Us</a>
+				</li>
+				<li>
+					<a href="/en-US/privacy/">Privacy Policy</a>
+				</li>
+				<li>
+					<a href="/en-US/about/legal.html">Legal Notices</a>
+				</li>
+				<li>
+					<a href="/en-US/legal/fraud-report/index.html">Report Trademark Abuse</a>
+				</li>
+			</ul>
+
+			<ul class="footer-nav">
+				<li>
+					<a href="http://twitter.com/firefox">Twitter</a>
+				</li>
+				<li>
+					<a href="http://facebook.com/Firefox">Facebook</a>
+				</li>
+				<li>
+					<a href="https://affiliates.mozilla.org/">Firefox Affiliates</a>
+				</li>
+			</ul>
+
+		</div>
+	</footer>
+
+</div>
+	<!-- JS live -->
+	<!-- // <script src="js/live.js"></script> -->
+	<!-- JS -->
+	<script src="//www.mozilla.org/tabzilla/media/js/tabzilla.js"></script>
+	<!--  <script src="https://raw.github.com/debloper/tabzilla/master/media/js/tabzilla.js"></script> -->
+	<!--  <script src="https://raw.github.com/debloper/tabzilla/master/media/js/locale.js"></script> -->
 </body>
 </html>
---
+++ main/README.md
@@ -0,0 +1,8 @@
+Mozilla-Hispano-FirefoxOS
+=========================
+
+Mozilla Hispano FirefoxOS Website
+
+
+### Preview:
+http://www.mozillafirefoxos.lseis.com/
